77a12bbe3e5bd57337c97f43d9ba09b64cc98f89
Philip Zubel <philip.zubel1@gmail.com>
2020-09-29 23:01:55 +0100

    Updates

---
+++ precache-manifest.2c4c253e0442f50054baf9eec558a98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1299962c1613ca20e068",
+    "url": "/SortingVisualiser/static/css/main.b592e770.chunk.css"
+  },
+  {
+    "revision": "1299962c1613ca20e068",
+    "url": "/SortingVisualiser/static/js/main.ff8f0db3.chunk.js"
+  },
+  {
+    "revision": "322b34779af98ee254e3",
+    "url": "/SortingVisualiser/static/js/runtime~main.be52a334.js"
+  },
+  {
+    "revision": "ffb2378a08ac8e39839a",
+    "url": "/SortingVisualiser/static/js/2.66a51b1d.chunk.js"
+  },
+  {
+    "revision": "43fb2510c7a33d13677096dab5352db3",
+    "url": "/SortingVisualiser/index.html"
+  }
+];
